16b96765db7333b53c7f855e9170a92df21f42e6
sistemasgrupomeekah-svg <sistemas.grupomeekah@gmail.com>
2025-11-17 12:36:04 -0300

    Atualização repositório para debug S3

--- core/settings.py
+++ core/settings.py
@@ -125,7 +125,7 @@ else:
     AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME', default='us-east-2') # Boa prática definir região
+    AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME', default='us-east-2')
     
     # 2. Configurações do S3
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
@@ -134,7 +134,7 @@ else:
     }
     
     # Lê do .env; se não encontrar, usa None como padrão (recomendado)
-    AWS_DEFAULT_ACL = env('AWS_DEFAULT_ACL', default=None)
+    # AWS_DEFAULT_ACL = env('AWS_DEFAULT_ACL', default=None)
     
     AWS_QUERYSTRING_AUTH = False 
 
@@ -145,7 +145,7 @@ else:
     
     # 4. MÍDIA (Uploads)
     # Usa o storage customizado em vendas/storage_backends.py
-    DEFAULT_FILE_STORAGE = 'vendas.storage_backends.MediaStorage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_prod')
